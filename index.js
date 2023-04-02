c8dfc911bba66043a0b8398623fd25e994b1ae5a
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-04-02 18:26:42 +0300

    added wallet

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const {
-    Connection,
-    PublicKey,
-    clusterApUrl,
-    Keypair,
-    LAMPORTS_PER_SQL
-} = require("@solana/web3.js")
+  Connection,
+  PublicKey,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} = require("@solana/web3.js");
 
-const wallet = new Keypair
+const wallet = new Keypair();
+
+const publicKey = wallet._keypair.publicKey;
+const secretKey = wallet._keypair.secretKey;
+
+console.log(publicKey);
