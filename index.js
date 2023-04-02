db360e2b8a395aefc949bd4f19595692288fe213
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-04-02 21:07:37 +0300

    final commit

--- index.js
+++ index.js
@@ -8,7 +8,33 @@ const {
 
 const wallet = new Keypair();
 
-const publicKey = wallet._keypair.publicKey;
+const publicKey = new PublicKey(wallet._keypair.publicKey);
 const secretKey = wallet._keypair.secretKey;
 
-console.log(publicKey);
+const getWalletBalance = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const walletBalance = await connection.getBalance(publicKey);
+    console.log("Wallet balance is${walletBalance}");
+  } catch (err) {
+    console.error(err);
+  }
+};
+const airDropSol = async () => {
+  try {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    const fromAirDropSignature = await connection.requestAirdrop(
+      publickey,
+      2 * LAMPORTS_PER_sOL
+    );
+    await connection.confirmTransaction(fromAirDropSignature);
+  } catch (err) {
+    console.log(err);
+  }
+};
+const main = async () => {
+  await getWalletBalance();
+  await airDropSol();
+};
+
+main();
